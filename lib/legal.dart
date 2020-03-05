aa1087eae2efaa247dd32af7b901113605eeb4de
Matt Mohandiss <mattmohandiss@gmail.com>
2020-03-05 15:00:54 -0500

    Initial commit

---
+++ lib/legal.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LegalPage extends StatefulWidget {
+  LegalPage({Key key}) : super(key: key);
+
+  @override
+  _LegalPageState createState() => _LegalPageState();
+}
+
+class _LegalPageState extends State<LegalPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
